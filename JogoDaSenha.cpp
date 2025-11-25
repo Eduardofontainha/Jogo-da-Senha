668c3ad4768a9237ef8a8739f01c784ccce93a16
Eduardo <eduardo.maia@estudante.ufjf.br>
2025-11-25 19:38:48 -0300

    Gerador de senha completo

--- JogoDaSenha.cpp
+++ JogoDaSenha.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
 using namespace std;
+#include <random>
+
 
 int geradorDeSenha(){
-    
+    random_device rd;
+    uniform_int_distribution<> distrib(1000, 9999);
+    mt19937 gen(rd());
+    int senha = distrib(gen);
+   
+    return senha;
+
 }
+int main(){
+    int senha = geradorDeSenha();
+    cout << senha << endl;
+}
+
