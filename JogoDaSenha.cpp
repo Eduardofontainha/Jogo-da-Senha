3b16d7aa2d31b46ff748dab6840e3fa8dfc8f529
Eduardo <eduardo.maia@estudante.ufjf.br>
2025-11-25 20:57:29 -0300

    1/3 do jogo

--- JogoDaSenha.cpp
+++ JogoDaSenha.cpp
@@ -5,15 +5,37 @@ using namespace std;
 
 int geradorDeSenha(){
     random_device rd;
-    uniform_int_distribution<> distrib(1000, 9999);
+    uniform_int_distribution<> distrib(1, 6);
     mt19937 gen(rd());
-    int senha = distrib(gen);
-   
+    int senha, d1 , d2, d3, d4, s1, s2, s3;
+    d1 = distrib(gen);
+    d2 = distrib(gen);
+    d3 = distrib(gen);
+    d4 = distrib(gen);
+    
+    d1 *=1000;
+    d2*=100;
+    d3*=10;
+
+    senha = d1 + d2 + d3 + d4;
     return senha;
 
+}
+int numeroDoJogador(){
+    int numero;
+    cin >> numero;
+    return numero;
 }
 int main(){
     int senha = geradorDeSenha();
     cout << senha << endl;
+    int numeroJogador = numeroDoJogador();
+
+    if((numeroJogador - 1000) < 0){
+        do{
+            numeroJogador = numeroDoJogador();
+        }while(numeroJogador - 1000 >= 0);
+    }
+
 }
 
